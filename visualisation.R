40779d2d2744bff8da9f8fb8240c931241fcaa39
NicolasSenave <39082613+NicolasSenave@users.noreply.github.com>
2018-05-21 12:40:14 +0200

    Update visualisation.R

--- visualisation.R
+++ visualisation.R
@@ -5,16 +5,16 @@
 ##' en fonction de x ou X est un echantillon de valeurs
 ##' et ou Y|X = x suit un modele logit de parametres (delta,beta).
 ##'
-##' @param echantillon numeric. Un echantillon de données.
+##' @param echantillon numeric. Un echantillon de donnees.
 ##' @param delta numeric. Parametre delta du modele logistique.
 ##' @param beta numeric. Parametre beta du modele logistique.
-##' @param nb_col numeric. Par défaut nb_col=10. Nombre de colonnes de l'histogramme.
+##' @param nb_col numeric. Par dÃ©faut nb_col=10. Nombre de colonnes de l'histogramme.
 ##'
 ##' @examples
 ##' echantillon <- c(rnorm(100))
 ##' visualisation(echantillon, 1.0, 2.0)
 ##'
-##' @return NULL. Affiche la représentation graphique.
+##' @return NULL. Affiche la reprÃ©sentation graphique.
 ##' @export
 ##'
 ##'
@@ -23,7 +23,7 @@
 visualisation <- function(echantillon,delta,beta,nb_col=10){
   hist(f_logit(echantillon,delta,beta),
        breaks = nb_col,
-       main="Distribution de Y conditionnellement à X=x",
+       main="Distribution theorique de Y conditionnellement a X=x",
        xlab="Valeurs x de l'echantillon X",
        ylab="Card(Y = 1|X=x)")
 }
