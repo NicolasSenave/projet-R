1ee0bb5d9b7c61492a58ebe380b74df388bf04e0
NicolasSenave <39082613+NicolasSenave@users.noreply.github.com>
2018-05-22 14:24:40 +0200

    Add files via upload
    Estimation encore à séparer en sous-scripts. Statbeta pas encore recopiée.

--- f_logit.R
+++ f_logit.R
@@ -18,5 +18,5 @@
 
 f_logit <- function(x,delta,beta){
   z = delta + beta*x
-  return(1-1/(1+exp(z)))
+  return(1 - 1/(1+exp(z)))
 }
--- visualisation.R
+++ visualisation.R
@@ -2,28 +2,38 @@
 ##'
 ##'
 ##' Represente graphiquement la distribution de Y conditionnellement a X = x 
-##' en fonction de x ou X est un echantillon de valeurs
-##' et ou Y|X = x suit un modele logit de parametres (delta,beta).
+##' en fonction de x ou X est un echantillon de valeurs explicatives
+##' et ou Y est un echantillon d'interet binaires.
 ##'
-##' @param echantillon numeric. Un echantillon de donnees.
-##' @param delta numeric. Parametre delta du modele logistique.
-##' @param beta numeric. Parametre beta du modele logistique.
-##' @param nb_col numeric. Par dÃ©faut nb_col=10. Nombre de colonnes de l'histogramme.
+##' @param X numeric. Un echantillon de donnees explicatives.
+##' @param Y numeric. Un echantillon de donnees binaires.
+##' @param nb_col numeric. Par défaut nb_col=10. Nombre de colonnes de l'histogramme.
 ##'
 ##' @examples
-##' echantillon <- c(rnorm(100))
-##' visualisation(echantillon, 1.0, 2.0)
+##' sim <- simulation(n,delta,beta)
+##' X <- as.numeric(unlist(sim[1]))
+##' Y <- as.numeric(unlist(sim[2]))
+##' visualisation(X,Y)
 ##'
-##' @return NULL. Affiche la reprÃ©sentation graphique.
+##' @return NULL. Affiche la représentation graphique.
 ##' @export
 ##'
 ##'
 
 
-visualisation <- function(echantillon,delta,beta,nb_col=10){
-  hist(f_logit(echantillon,delta,beta),
+visualisation <- function(X,Y,nb_col=10){
+  n = length(X)
+  distribution_y <- c()
+  k = 0
+  for (i in (1:n)){
+    if (Y[i] == 1){
+      distribution_y[k] <- X[i]
+      k = k+1
+    }
+  }
+  hist(distribution_y,
        breaks = nb_col,
-       main="Distribution theorique de Y conditionnellement a X=x",
+       main="Distribution de Y conditionnellement a X=x",
        xlab="Valeurs x de l'echantillon X",
        ylab="Card(Y = 1|X=x)")
 }
