58025dff109d55a3f8c97111dd64b65c0f7fa7b6
QuentinNouvellon <39522522+QuentinNouvellon@users.noreply.github.com>
2018-05-22 14:22:22 +0200

    Update base_donnees_R.R

--- base_donnees_R.R
+++ base_donnees_R.R
@@ -1,13 +1,10 @@
-base <- read.table("http://freakonometrics.free.fr/base-glm-act2040.txt", header=TRUE)
-base2 <- read.table("http://freakonometrics.free.fr/base-pratique-act2040.txt", header=TRUE)
 
-
-# Expliquer le lien entre le nombre de pages vu par un lecteur sur un service d'information en ligne et sa volonté à renouveller son abonnement
+# Expliquer le lien entre le nombre de pages vu par un lecteur sur un service d'information en ligne et sa volontÃ© Ã  renouveller son abonnement
 
 donnees <- read.table(file = 'demoLOGf.csv', sep=",",  header=F)
-# X1 = nombre de pages vu par semaine en moyenne sur les 10 dernières semaines
+# X1 = nombre de pages vu par semaine en moyenne sur les 10 derniÃ¨res semaines
 X1 <- donnees[,3]
-# X2 = nombre de pages vu au cours de la dernière semaine
+# X2 = nombre de pages vu au cours de la derniÃ¨re semaine
 X2 <- donnees[,4]
-# Y = (1 si volonté de renouvellement de l'abonnement) (0 sinon)
+# Y = (1 si volontÃ© de renouvellement de l'abonnement) (0 sinon)
 Y <- donnees[,5]
