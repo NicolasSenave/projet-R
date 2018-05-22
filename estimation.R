4dceb6a0cd1a3249c8558b29c4a528996cd8ae17
NicolasSenave <39082613+NicolasSenave@users.noreply.github.com>
2018-05-22 15:38:01 +0200

    Add files via upload

--- estimation.R
+++ estimation.R
@@ -6,8 +6,8 @@
 ##' X (donnees explicatives) et Y (reponses binaires), 
 ##' par un algorithme de Newton-Raphson.
 ##'
-##' @param X numeric. L'echantillon des donnees explicatives.
-##' @param Y numeric. L'echantillon des reponses binaires.
+##' @param x numeric. L'echantillon des donnees explicatives.
+##' @param y numeric. L'echantillon des reponses binaires.
 ##'
 ##' @examples
 ##' sim <- simulation(100, 1.0, 2.0)
@@ -21,9 +21,22 @@
 ##'
 
 
-estimation <- function(X, Y){
-  delta <- 0.0
-  beta <- 0.0
-  # ...
-  return(c(delta,beta))
+estimation <- function(x,y){
+  delta = 0
+  beta = 0
+  v0 = matrix(c(delta,beta),nrow=2)
+  v1 = v0 - inverse_fonction(hess_logit(x,y,v0[1],v0[2]))%*%grad_logit(x,y,v0[1],v0[2])
+  grad1 = grad_logit(x,y,v1[1],v1[2])
+  grad2 = grad_logit(x,y,v0[1],v0[2])
+  i = 0
+  while( norme(grad1-grad2)>10**-20 & i<30000 ){
+    v0 = v1
+    v1 = v0-inverse_fonction(hess_logit(x,y,v0[1],v0[2]))%*%grad_logit(x,y,v0[1],v0[2])
+    grad1 = grad_logit(x,y,v1[1],v1[2])
+    grad2 = grad_logit(x,y,v0[1],v0[2])
+    i = i+1
+  }
+  delta = v1[1]
+  beta = v1[2]
+  return(c(delta,beta))             
 }
---
+++ hess_logit.R
@@ -0,0 +1,30 @@
+###################################################################################
+##'
+##'
+##' Renvoie la hessienne de la log-varisemblance 
+##' du modele logit de parametres (delta,beta)
+##' sur les donnees explicatives x et les donnees d'interet binaires y.
+##'
+##' @param x numeric. L'echantillon des donnees explicatives.
+##' @param y numeric. L'echantillon des reponses binaires.
+##' @param delta numeric. Parametre delta du modele logistique.
+##' @param beta numeric. Parametre beta du modele logistique.
+##'
+##' @examples
+##' sim <- simulation(100, 1.0, 2.0)
+##' X <- as.numeric(unlist(sim[1]))
+##' Y <- as.numeric(unlist(sim[2]))
+##' hess_logit(X,Y, 1.0, 2.0)
+##'
+##' @return matrix.
+##'
+##'
+
+hess_logit <- function(x,y,delta,beta){
+  f_logit_x = f_logit(x,delta,beta)
+  composante = (1 - f_logit_x) * f_logit_x
+  d1 = sum(composante)
+  d2 = sum(x* composante)
+  d3 = sum(x**2 * composante)
+  return ( matrix(c(-d1,-d2,-d2,-d3),nrow=2) )
+}
